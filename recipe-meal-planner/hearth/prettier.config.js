eb19fc625606ff995dc2a166fc4a749ffef5dd4f
Nathan Brusher <natebrusher@gmail.com>
2025-06-27 17:17:11 -0500

    Set up Svelte with Tailwind, Prettier, ShadCN, Chart.js

---
+++ recipe-meal-planner/hearth/prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    plugins: ["prettier-plugin-svelte"],
+    semi: true,
+    singleQuote: true,
+    svelteSortOrder: "scripts-markup-styles",
+    svelteStrictMode: true,
+  };
---
+++ recipe-meal-planner/hearth/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<p class="text-green-500 text-3xl font-bold">
+    Tailwind is working!
+  </p>
